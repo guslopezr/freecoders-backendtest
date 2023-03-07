9e0334d5010a75f6c75ae957d19f1ab3a7a558d2
guslopezr <guslopezr@gmail.com>
2023-03-06 21:44:27 -0300

    se agrega método post para ingresar usuarios en el endpoint

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar PostgreSQL pool
 const pool = new Pool({
@@ -15,7 +19,7 @@ const pool = new Pool({
 app.get('/users', async(req, res) => {
     try {
         // Query 
-        const result = await pool.query('SELECT nombre, apellido, email, area, repositorio_url, linkedin, resenha, telefono, portafolio, presupuesto, oferta_valor, valor_hora FROM programadores');
+        const result = await pool.query('SELECT nombre, apellido, clave, foto_url, edad, email, area, repositorio_url, linkedin, resenha, telefono, portafolio, presupuesto, oferta_valor, valor_hora FROM programadores');
         const users = result.rows;
 
         // Respuesta JSON
@@ -49,6 +53,21 @@ app.get('/users/:id', async(req, res) => {
         res.status(500).send('Internal server error');
     }
 });
+
+// Definir endpoint para añidar
+app.post('/users', async(req, res) => {
+    const { nombre, apellido, clave, foto_url, edad, email, area, repositorio_url, linkedin, resenha, telefono, portafolio, presupuesto, oferta_valor, valor_hora } = req.body;
+
+    try {
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO programadores (nombre, apellido, clave, foto_url, edad, email, area, repositorio_url, linkedin, resenha, telefono, portafolio, presupuesto, oferta_valor, valor_hora) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)', [nombre, apellido, clave, foto_url, edad, email, area, repositorio_url, linkedin, resenha, telefono, portafolio, presupuesto, oferta_valor, valor_hora]);
+        res.send(`User ${nombre} ${apellido} added with ID ${result.insertId}`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error al agregar usuario');
+    }
+});
+
 
 // Start server
 app.listen(3000, () => {
